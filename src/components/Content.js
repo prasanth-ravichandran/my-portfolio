7ced45f356486280322813940503de2e85633a24
prasanth-ravichandran <132450380+prasanth-ravichandran@users.noreply.github.com>
2023-07-26 22:47:40 +0530

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -138,7 +138,7 @@ function Content() {
         </h1>
         <motion.a
           className="btn btn-light"
-          href="https://drive.google.com/file/d/1Mu4A-x4PkiQalEAHcnfV4pduphNF1z3g/view?usp=sharing"
+          href="https://drive.google.com/file/d/1VuQapKMBhzyILUmdp4ps-sALKZ3zVoFD/view?usp=sharing"
           style={{
             textDecoration: "none",
             fontFamily: "initial",
