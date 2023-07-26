8a8c10d0447e6f282cdaadac6e06573f9be499d7
prasanth-ravichandran <132450380+prasanth-ravichandran@users.noreply.github.com>
2023-07-26 22:21:43 +0530

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -214,7 +214,7 @@ function Project() {
                   className="col-4 mt-5 mb-5"
                   whileHover={{ scale: 1.2 }}
                 >
-                  <a href="https://github.com/prasanth-ravichandran/crud">
+                  <a href="	https://student-portal-site.vercel.app">
                     {" "}
                     <img
                       src={CRUDOperation}
@@ -233,7 +233,7 @@ function Project() {
                   className="col-4 mt-5 mb-5"
                   whileHover={{ scale: 1.2 }}
                 >
-                  <a href="https://github.com/prasanth-ravichandran/Projectreact">
+                  <a href="	https://e-commerce-shop-prasanth.netlify.app">
                     {" "}
                     <img
                       src={ECOMMERCE}
